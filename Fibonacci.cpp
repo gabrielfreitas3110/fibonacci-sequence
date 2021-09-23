f6aa20e6db9495f7e35f31718df95039260be0ed
gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-09-23 20:58:28 -0300

    Better logic, last commit

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -2,8 +2,7 @@
 int fib(int n) {                                          
 	if(n <= 1)
 		return n;
-	if(n > 1)
-		return (fib(n-1) + fib(n-2));     
+	return (fib(n-1) + fib(n-2));     
 }
 int main(void) {
 	int n;
